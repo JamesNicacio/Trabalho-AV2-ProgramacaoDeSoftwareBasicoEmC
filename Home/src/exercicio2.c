8b69a168e97916ce54bd9d38f0ed68a63c87fc16
JamesNicacio <jamessonnicacio@gmail.com>
2023-05-24 11:33:07 -0300

    update exercicio2.c

--- Home/src/exercicio2.c
+++ Home/src/exercicio2.c
@@ -14,8 +14,8 @@ Nota: O array não tem números repetidos.
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include <time.h>
 
 void printArray(int array[], int size) {
@@ -25,6 +25,7 @@ void printArray(int array[], int size) {
   printf("\n");
 }
 
+
 int main(int argc, char const *argv[]){
     int vetor[1000];
     srand(time(NULL));   // Initialization, should only be called once.
@@ -33,10 +34,8 @@ int main(int argc, char const *argv[]){
     // random int between 0 and 19
     // int r = rand() % 20;
     int contains = 0;
-    int totalIteracoes = 0;
 
     for (int i = 0; i < 1000;){
-        totalIteracoes++;
         // int r = rand() % 20;
         // vetor[i] = r;
         // printf("vetor[%d]::%d\n", i, vetor[i]);
@@ -67,5 +66,53 @@ int main(int argc, char const *argv[]){
 
     printArray(vetor, size);
     printf("Size array::%d\n", size);
+
+    for(int i = 0; i <= 4; i++){
+        pid_t child_pid = fork();
+        if(child_pid == 0){
+            // printf("\nPAI[%d] - FILHO .Processo filho.PID[%d]: |%d|\n", getppid(), i, getpid());
+
+            //  Define posição inicial e fim para procurar no array.
+            int start = i * 200;
+            int finish = start + 199;
+
+            printf("filho %d - start[%d] - finish[%d]\n", i +1, start, finish);
+            
+            for (int j = start; j < finish; j++) {
+                if(vetor[j] == 13){ // Procura se o vetor possui o seguinte valor.
+                    printf("Filho %d encontrou o número na posição %d\n", i + 1, j);
+                    exit(i +1);
+                }
+            }
+            exit(0);
+        }
+
+    }
+
+
+    int status;
+    int found_by_child = 0;
+    int child_number = 0;
+
+    for (int i = 1; i < 5; i++) {
+        wait(&status);
+
+        // WIFEXITED() is true if the process exited normally via a call to exit. 
+        // WEXITSTATUS() if WIFEXITED is true, this returns the value passed to exit by the child.
+        if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+            // Este filho encontrou o número.
+            found_by_child = WEXITSTATUS(status);
+            child_number = i +1;
+            break;
+        }
+    }
+
+    if (found_by_child != 0) {
+        printf("O número foi encontrado pelo filho [%d]\n", found_by_child);
+    } else {
+        printf("O número não foi encontrado\n");
+    }
+
+
 
 }
